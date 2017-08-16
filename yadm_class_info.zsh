cabfd87af5714e50efe03176457c4f77edd79cbb
Chris Olin <chris.olin@cloudcheckr.com>
2017-08-16 10:57:15 -0400

    initial commit

---
+++ yadm_class_info.zsh
@@ -0,0 +1,8 @@
+# Outputs current yadm class value
+function yadm_class_info() {
+	local ref
+	if [[ -n "$(command grep class ~/.yadm/repo.git/config | awk '{print $3}')" ]]; then
+		yadm_class=$(command grep class ~/.yadm/repo.git/config | awk '{print $3}')
+	fi
+	echo "($yadm_class)"
+}
