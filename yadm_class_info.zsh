1c46103f8f12f676b5c0e118135dea0a2779a3a1
Chris Olin <contact@chrisolin.com>
2021-07-17 17:55:32 -0400

    now v2 compatible

--- yadm_class_info.zsh
+++ yadm_class_info.zsh
@@ -1,7 +1,7 @@
 # Outputs current yadm class value
 function yadm_class_info() {
 	local ref
-	if [[ -n "$(command grep class ~/.yadm/repo.git/config | awk '{print $3}')" ]]; then
+	if [[ -n "$(command grep class ~/.config/yadm/repo.git/config | awk '{print $3}')" ]]; then
 		yadm_class=$(command grep class ~/.yadm/repo.git/config | awk '{print $3}')
 	fi
 	echo "($yadm_class)"
