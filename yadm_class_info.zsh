662114b4cdd0803b138529a99644ac50028d2e14
Chris Olin <contact@chrisolin.com>
2021-07-17 17:59:04 -0400

    missed a line, derp

--- yadm_class_info.zsh
+++ yadm_class_info.zsh
@@ -2,7 +2,7 @@
 function yadm_class_info() {
 	local ref
 	if [[ -n "$(command grep class ~/.config/yadm/repo.git/config | awk '{print $3}')" ]]; then
-		yadm_class=$(command grep class ~/.yadm/repo.git/config | awk '{print $3}')
+		yadm_class=$(command grep class ~/.config/yadm/repo.git/config | awk '{print $3}')
 	fi
 	echo "($yadm_class)"
 }
